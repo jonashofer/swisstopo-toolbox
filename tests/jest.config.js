f8a9db569a8c0731f6b73ec6da5efbfd25732aed
Jonas Hofer <jonashofer@bluemail.ch>
2025-02-04 13:07:04 +0000

    update to oblique 11

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -3,10 +3,6 @@
 module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/tests/setupJest.ts"],
-  globals: {
-    "ts-jest": {
-      diagnostics: false,
-    },
   },
   collectCoverage: true,
 };
