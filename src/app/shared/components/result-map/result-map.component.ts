0df0c7616d372f8d7bd803bd6d9e7bfb8ebdfc4b
Jonas Hofer <jh@nexplore.ch>
2025-02-05 15:35:40 +0100

    fix map extent, do not use transform

--- src/app/shared/components/result-map/result-map.component.ts
+++ src/app/shared/components/result-map/result-map.component.ts
@@ -4,17 +4,16 @@ import { Feature, View } from 'ol';
 import { Tile as TileLayer } from 'ol/layer';
 import { XYZ } from 'ol/source';
 import { FullScreen } from 'ol/control';
-import VectorSource, { VectorSourceEvent } from 'ol/source/Vector';
+import VectorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
 import Point from 'ol/geom/Point';
-import { fromLonLat, transformExtent } from 'ol/proj';
-import { Fill, Icon, Stroke, Style } from 'ol/style';
+import { fromLonLat } from 'ol/proj';
+import { Icon, Style } from 'ol/style';
 import { AddressCoordinateTableEntry } from '../../models/AddressCoordinateTableEntry';
 import { filter } from 'rxjs/operators';
 import { DownloadService } from '../../services';
 import { StorageService } from '../../services/storage.service';
 import { MapInteractionService } from '../../services/map-interaction.service';
-import CircleStyle from 'ol/style/Circle';
 import { Geometry } from 'ol/geom';
 import { MatDialog } from '@angular/material/dialog';
 
@@ -40,7 +39,7 @@ const layers = [
 
 const svg = (hexFill: string, hexStroke: string) =>
   encodeURIComponent(`
-<svg version="1.1" id="marker" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+<svg version="1.1" id="marker" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
 	width="40" height="40" viewBox="0 0 256 256" xml:space="preserve">
 	<style type="text/css">
 		<![CDATA[
@@ -74,44 +73,16 @@ const selectedIconStyle = new Style({
     src: svgSrc("#ffffff", '#fa011c')
   })
 });
-// const markerStyle = new Style({
-//   image: new Icon({
-//     anchor: [0.5, 1],
-//     src: 'https://map.geo.admin.ch/f13b978/img/marker.png'
-//   })
-// })
-
-// const selectedMarkerStyle = new Style({
-//   image: new Icon({
-//     anchor: [0.5, 1],
-//     src: 'https://map.geo.admin.ch/f13b978/img/marker.png',
-//     color: '#ffff00'
-//   })
-// })
-
-const circleStyle = new Style({
-  image: new CircleStyle({
-    radius: 8,
-    fill: new Fill({
-      color: [255, 255, 0, 0.5]
-    }),
-    stroke: new Stroke({
-      color: [255, 140, 0, 1],
-      width: 3
-    })
-  })
-});
 
 const markerLayer = new VectorLayer({
   style: iconStyle
 });
 
 const view = new View({
-  projection: 'EPSG:3857',
   maxZoom: 20,
   constrainOnlyCenter: true,
   minZoom: 7.5,
-  extent: transformExtent([5.6, 48, 11, 45], 'EPSG:4326', 'EPSG:3857'),
+  extent: [609050.241376, 5719527, 1200978.588417, 6372035],
   enableRotation: false
 });
 
@@ -163,7 +134,7 @@ export class ResultMapComponent implements AfterViewInit, OnDestroy {
 
   ngAfterViewInit() {
     if (this.mapDiv) {
-      this.map = new Map({
+			this.map = new Map({
         controls: [new FullScreen()],
         layers: this.getLayers(),
         view,
